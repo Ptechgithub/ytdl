f0fc342b2a04af176258397b8d6b7bd57e57a1fb
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-10-31 23:43:07 +0330

    Update config.py

--- config.py
+++ config.py
@@ -12,9 +12,9 @@ import os
 # general settings
 WORKERS: int = int(os.getenv("WORKERS", 100))
 PYRO_WORKERS: int = int(os.getenv("PYRO_WORKERS", min(64, (os.cpu_count() + 4) * 10)))
-APP_ID: int = int(os.getenv("APP_ID", 11111))
-APP_HASH = os.getenv("APP_HASH", "xxxxx")
-TOKEN = os.getenv("TOKEN", "xxxxxx")
+APP_ID: int = int(os.getenv("APP_ID", A))
+APP_HASH = os.getenv("APP_HASH", "B")
+TOKEN = os.getenv("TOKEN", "C")
 
 REDIS = os.getenv("REDIS", "redis")
 
